56c4fc68e43bd1aa6f7e31684990de6ecde3f48d
JiHyung Lee <jhlee.8804@gmail.com>
2015-10-23 14:33:44 +0900

    added Explorer Sample

---
+++ Explorer/FolderBar.h
@@ -0,0 +1,42 @@
+// This is a part of the Microsoft Foundation Classes C++ library.
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+//
+// This source code is only intended as a supplement to the
+// Microsoft Foundation Classes Reference and related
+// electronic documentation provided with the library.
+// See these sources for detailed information regarding the
+// Microsoft Foundation Classes product.
+
+#pragma once
+
+/////////////////////////////////////////////////////////////////////////////
+// CFolderBar window
+
+class CFolderBar : public CDockablePane
+{
+// Construction
+public:
+	CFolderBar();
+
+// Attributes
+public:
+	CMFCShellTreeCtrl	m_wndShellTree;
+
+// Operations
+public:
+
+// Overrides
+
+// Implementation
+public:
+	virtual ~CFolderBar();
+
+protected:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnSetFocus(CWnd* pOldWnd);
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+
+	DECLARE_MESSAGE_MAP()
+};
